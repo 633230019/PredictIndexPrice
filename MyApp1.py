8544d785bf1c97d6e83537f50552fafb5bf8a930
633230019 <144575516+633230019@users.noreply.github.com>
2023-10-01 10:23:34 +0700

    .commit

--- MyApp1.py
+++ MyApp1.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import streamlit as st
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,mean_squared_error
 
 st.title("Index Price Prediction")
 st.header("Index Price Prediction from NPRU")
