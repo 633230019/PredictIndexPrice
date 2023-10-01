4056863c775d6c3667569f3750661731a7cdc525
633230019 <144575516+633230019@users.noreply.github.com>
2023-10-01 10:26:00 +0700

    .commit

--- MyApp1.py
+++ MyApp1.py
@@ -14,3 +14,19 @@ st.write(df.head(10))
 
 st.header("Prediction Chart")
 st.line_chart(df, x="interest_rate",y=["unemployment_rate","stock_index_price"])
+
+x=df[['interest_rate','unemployment_rate']]
+y=df['stock_index_price']
+pf=PolynomialFeatures(degree=3)
+x_poly=pf.fit_transform(x)
+
+x_train,x_test,y_train,y_test =train_test_split(x_poly,y,random_state=0)
+
+modelRegress=LinearRegression()
+modelRegress.fit(x_train,y_train)
+
+x_input=[[2,5],[2.2,5.7]]
+
+y_predict=modelRegress.predict(pf.fit_transform(x_input))
+
+st.write(y_predict)
